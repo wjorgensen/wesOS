1acd4450f719e14d719e415bf60825abcaa49a5e
Wes Jorgensen <wjorgensen29@gmail.com>
2021-06-04 09:46:49 -0500

    Finished Makefile

--- src/impl/x86_64/boot/header.asm
+++ src/impl/x86_64/boot/header.asm
@@ -1,13 +1,13 @@
 section .multiboot_header
 header_start:
     ; magic number
-    dd 0xe85250d86 ; multiboot 2
+    dd 0xe85250d6 ; multiboot 2
     ; architecture
     dd 0 ; protected mode i386
     ; header length
     dd header_end - header_start 
     ; checksum
-    dd 0x100000000 - (0xe85250d86 + 0 + (header_end - header_start))
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
 
     ;end tag
     dw 0
