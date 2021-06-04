b38dd79060bf723f768b71196a1ddade8c2ed9e1
Wes Jorgensen <wjorgensen29@gmail.com>
2021-06-04 11:02:36 -0500

    added long mode cpu and paging

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -3,6 +3,120 @@ global start
 section .text
 bits 32
 start:
+    mov esp, stack_top
+    
+    call check_multiboot
+    call check_cpuid
+    call check_long_mode
+
+    call setup_page_tables
+    call enable_paging
+
     ; print "OK"
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
+
+check_multiboot:
+    cmp eax, 0x36d76289
+    jne .no_multiboot
+    ret
+.no_multiboot:
+    mov al, "M"
+    jmp error
+
+check_cpuid:
+    pushf
+    pop eax
+    mov ecx, eax
+    xor eax, 1 << 21
+    push eax
+    popfd
+    pushfd
+    pop eax
+    push ecx
+    popfd
+    cmp eax, ecx
+    je .no_cpuid
+    ret
+.no_cpuid:
+    mov al, "C"
+    jmp error
+
+check_long_mode:
+    mov eax, 0x80000000
+    cpuid
+    cmp eax, 0x80000001
+    jb .no_long_mode
+
+    mov eax, 0x80000001
+    cpuid
+    test edx, 1 << 29
+    jz .no_long_mode
+
+    ret
+.no_long_mode
+    mov al, "L"
+    jmp error
+setup_page_tables:
+    mov eax, page_table_l3
+    or eax, 0b11 ; present, writable
+    mov [page_table_14], eax
+
+    mov eax, page_table_l2
+    or eax, 0b11 ; present, writable
+    mov [page_table_14], eax
+.loop:
+
+    mov eax, 0x200000, 2MiB
+    mul ecx
+    or eax, 0b10000011 ; present, writable, huge page
+    mov [page_table_l2 + ecx * 8], eax
+
+    inc ecx, 0 ; counter
+    cmp ecx, 512 ; checks if the whole table is mapped
+    jne .loop ; if not, continue
+
+    ret
+
+enable_paging:
+    ; pass page table location to cpu
+    mov eax, page_table_14
+    mov cr3, eax
+
+    ;enable PAE
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; enable long mode 
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; enable paging
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+
+    ret
+
+error:
+    ; print "Err: X" where X is the error code
+    mov dword [0xb8000], 0x4f524f45
+    mov dword [0xb8000], 0x4f3a4f52
+    mov dword [0xb8000], 0x4f204f20
+    mov byte  [0xb8000], al
+    hlt
+
+section .bss
+align 4096
+page_table_14:
+    resb4096
+page_table_13:
+    resb4096
+page_table_12:
+    resb4096
+stack_bottom:
+    resb 4096 * 4
+stack_top:
